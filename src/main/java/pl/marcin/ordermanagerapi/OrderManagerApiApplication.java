206d7498f733ab2165958e861482a7575b35a87f
Marcin Weklar <weklar10@gmail.com>
2023-02-06 23:03:47 +0100

    first commit

---
+++ src/main/java/pl/marcin/ordermanagerapi/OrderManagerApiApplication.java
@@ -0,0 +1,13 @@
+package pl.marcin.ordermanagerapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderManagerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderManagerApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/marcin/ordermanagerapi/OrderManagerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.marcin.ordermanagerapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderManagerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
